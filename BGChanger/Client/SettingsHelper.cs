0096aa6633eac1bc44cf05e4ac9f0d0feac9c666
Vlad Zaharia <vlad@zaharia.io>
2016-03-10 21:14:14 -0800

    Initial client commit - Basic UI

---
+++ BGChanger/Client/SettingsHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace BGChanger.Client
+{
+    internal static class SettingsHelper
+    {
+        public static Windows.Storage.ApplicationDataContainer LocalSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+        public static string Url
+        {
+            get
+            {
+                if (!LocalSettings.Values.ContainsKey("URL"))
+                {
+                    LocalSettings.Values["URL"] = "http://bg.zaharia.io/";
+                }
+
+                return LocalSettings.Values["URL"].ToString();
+            }
+        }
+
+        public static string ApiKey
+        {
+            get
+            {
+                if (!LocalSettings.Values.ContainsKey("APIKey"))
+                {
+                    Guid rawGuid = Guid.NewGuid();
+                    String base64Guid = Convert.ToBase64String(rawGuid.ToByteArray())
+                                               .Substring(0, 22)
+                                               .Replace("/", "_")
+                                               .Replace("+", "-");
+
+                    LocalSettings.Values["APIKey"] = base64Guid;
+                }
+
+                return LocalSettings.Values["APIKey"].ToString();
+            }
+        }
+
+        public static DateTime LastUpdatedDate => DateTime.UtcNow;
+
+        public static bool UpdateWallpaper
+        {
+            get
+            {
+                if (!LocalSettings.Values.ContainsKey("UpdateWallpaper"))
+                {
+                    LocalSettings.Values["UpdateWallpaper"] = false;
+                }
+
+                return (bool)LocalSettings.Values["UpdateWallpaper"];
+            }
+
+            set { LocalSettings.Values["UpdateWallpaper"] = value; }
+        }
+
+        public static bool UpdateLockScreen
+        {
+            get
+            {
+                if (!LocalSettings.Values.ContainsKey("UpdateLockScreen"))
+                {
+                    LocalSettings.Values["UpdateLockScreen"] = false;
+                }
+
+                return (bool)LocalSettings.Values["UpdateLockScreen"];
+            }
+
+            set { LocalSettings.Values["UpdateLockScreen"] = value; }
+        }
+    }
+}
---
+++ BGChanger/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using BGChanger.Client;
+
+namespace BGChanger
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            UpdateValues();
+        }
+
+        public void UpdateValues()
+        {
+            ServerUrl.Text = SettingsHelper.Url;
+            ApiKey.Text = SettingsHelper.ApiKey;
+            LastUpdatedDate.Text = SettingsHelper.LastUpdatedDate.ToString(CultureInfo.InvariantCulture);
+
+            UpdateWallpaper.IsOn = SettingsHelper.UpdateWallpaper;
+            UpdateLockScreen.IsOn = SettingsHelper.UpdateLockScreen;
+        }
+
+        /**
+         * Event Handlers
+         */
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.IsPaneOpen = !MainContent.IsPaneOpen;
+        }
+
+        private void UpdateWallpaper_OnToggled(object sender, RoutedEventArgs e)
+        {
+            var toggle = (ToggleSwitch)sender;
+            SettingsHelper.UpdateWallpaper = toggle.IsOn;
+        }
+
+        private void UpdateLockScreen_OnToggled(object sender, RoutedEventArgs e)
+        {
+            var toggle = (ToggleSwitch)sender;
+            SettingsHelper.UpdateLockScreen = toggle.IsOn;
+        }
+    }
+}
