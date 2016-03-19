05cb34b9148e28958bb0bfc3557cbf4a3490d619
Vlad Zaharia <vlad@zaharia.io>
2016-03-19 12:32:37 -0700

    Update version to 0.1 (pre-Alpha dev version) and assembly information for the app.

--- Floodlight-Client/Properties/AssemblyInfo.cs
+++ Floodlight-Client/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Floodlight-Client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Floodlight Client Library")]
+[assembly: AssemblyDescription("Provides connectivity and common functionality for the Floodlight app.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Floodlight")]
 [assembly: AssemblyProduct("Floodlight-Client")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright 2016 Vlad Zaharia")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: ComVisible(false)]
